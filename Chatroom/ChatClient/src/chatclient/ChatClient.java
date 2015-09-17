f270419ce366f51ffd7281d138de041b7676795c
Kathryn Hodge <kahodge@vassar.edu>
2015-09-17 19:02:37 -0400

    Setting up chat client :)
    *tom also visited

--- Chatroom/ChatClient/src/chatclient/ChatClient.java
+++ Chatroom/ChatClient/src/chatclient/ChatClient.java
@@ -9,6 +9,16 @@ import java.net.*;
 
 public class ChatClient extends JFrame  implements ActionListener {
 
+    // Sockets & Streams
+    private static InetAddress host;
+    private static final int PORT = 52946;
+    private static Socket socket;
+    private static Scanner networkInput;
+    private static PrintWriter networkOutput;
+    
+    
+    
+    // UI
     private static ChatClient frame;
     private JTextArea chatArea;
     private JTextArea messageArea;
@@ -16,12 +26,24 @@ public class ChatClient extends JFrame  implements ActionListener {
     private String message = "";
 
     public static void main(String[] args) {
-
+        try {
+            host = InetAddress.getLocalHost();
+        } catch (UnknownHostException uhEx) {
+            System.out.println("Host ID not found!");
+            System.exit(1);
+        }
+        
         frame = new ChatClient();
+        frame.setUpStreams();
         frame.setTitle("A Simple Chat Client");
         frame.setSize(400, 500);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        while(true) {
+           frame.readMessage();
+        }
+        
     }
 
 
@@ -51,13 +73,10 @@ public class ChatClient extends JFrame  implements ActionListener {
                                                     JOptionPane.PLAIN_MESSAGE);
         System.out.println("User name dialog input was: " + handle);
 
-        // Code to connect with server and set up client
+       // Code to connect with server and set up client
         // thread for receiving and displaying messages
         // from server.
-
-        // The line below simply demonstrates how to write text
-        // into the chat area.
-        chatArea.append("Message written into chat area.");
+        setUpStreams();
     }
 
     public void actionPerformed(ActionEvent event) {
@@ -68,9 +87,58 @@ public class ChatClient extends JFrame  implements ActionListener {
             messageArea.setText(""); 
             if (message.equals("Bye")) {
                 // Code to close connection if user typed "Bye".
+                try {
+                    System.out.println("Closing connection...");
+                    socket.close();
+                } catch (IOException ioEx) {
+                    System.out.println("Unable to disconnect!");
+                    System.exit(1);
+                }
             } else {
                 // Code to send any other message to the server.
+                sendMessage();
             }
+        }
+    }
+    
+    // Code to connect with server and set up client
+    // thread for receiving and displaying messages
+    // from server.
+    public void setUpStreams() {
+         try {
+             // Making a socket
+            socket = new Socket(host, PORT);
+            // Setting up I/O Streams
+            networkInput = new Scanner(socket.getInputStream());
+            networkOutput = new PrintWriter(socket.getOutputStream(), true);
+            
+            // Let's get some chat messages!
+//             String message, response;
+//            do {
+//                System.out.print("Enter message ('QUIT' to exit): ");
+//                message = userEntry.nextLine();
+//                networkOutput.println(message);
+//                response = networkInput.nextLine();
+//                System.out.println("SERVER> " + response);
+//            } while (!message.equals("QUIT"));
+            
+            
+         } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+        }
+    }
+    
+    public void sendMessage() {
+        System.out.println("Sending Message...");
+        networkOutput.println(message);
+    }
+    
+    public void readMessage() {
+        // getting a response
+        String response = networkInput.nextLine();
+        if (!response.isEmpty()) {
+            // appending to the chat area
+            chatArea.append(response);
         }
     }
 
